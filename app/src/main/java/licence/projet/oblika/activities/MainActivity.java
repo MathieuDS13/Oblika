c6933f979810b94213387383f949cb1e278cc786
TechnoShark13 <mathieu.dasilva13@gmail.com>
2020-05-04 16:28:29 +0200

    Ajout du bouton pour lancer la GameActivity

--- app/src/main/java/licence/projet/oblika/activities/MainActivity.java
+++ app/src/main/java/licence/projet/oblika/activities/MainActivity.java
@@ -1,7 +1,10 @@
 package licence.projet.oblika.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import licence.projet.oblika.R;
 
@@ -11,6 +14,15 @@ public class  MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonGameActivity = findViewById(R.id.buttonGameActivity);
+        buttonGameActivity.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent activity2Intent = new Intent(getApplicationContext(), GameActivity.class);
+                startActivity(activity2Intent);
+            }
+        });
     }
 
 }
