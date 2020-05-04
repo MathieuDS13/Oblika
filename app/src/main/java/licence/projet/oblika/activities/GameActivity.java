5a7853ee8af6d57b4e7329eae8556dcfe48fe594
JaiPasDidee <andrea.pires@etu.univ-amu.fr>
2020-05-04 17:17:42 +0200

    ajout de l'accelerometre dans la GameActivity

--- app/src/main/java/licence/projet/oblika/activities/GameActivity.java
+++ app/src/main/java/licence/projet/oblika/activities/GameActivity.java
@@ -1,15 +1,24 @@
 package licence.projet.oblika.activities;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.SensorManager;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+
+import licence.projet.oblika.engine.utils.Accelerometer;
 
 public class GameActivity extends Activity {
     private GLSurfaceView gLView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        Accelerometer accelerometer = new Accelerometer();
+
         super.onCreate(savedInstanceState);
+
+        SensorManager sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        sensorManager.registerListener(accelerometer, SensorManager.SENSOR_ACCELEROMETER);
 
         gLView = new GLView(this);
         setContentView(gLView);
