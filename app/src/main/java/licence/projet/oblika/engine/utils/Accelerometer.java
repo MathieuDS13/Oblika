3b1d84858a72253fd934139540a3d1f6115acdab
JaiPasDidee <andrea.pires@etu.univ-amu.fr>
2020-05-04 17:06:16 +0200

    implementation de la classe

--- app/src/main/java/licence/projet/oblika/engine/utils/Accelerometer.java
+++ app/src/main/java/licence/projet/oblika/engine/utils/Accelerometer.java
@@ -1,4 +1,15 @@
 package licence.projet.oblika.engine.utils;
 
 public class Accelerometer {
+    float x, y, z;
+
+    public void onSensorChanged(int sensor, float[] values) {
+
+        x = values[0];
+
+        y = values[1];
+
+        z = values[2];
+
+    }
 }
