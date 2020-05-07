63f105746aba397694155ca8c134d08f0f7b42f5
JaiPasDidee <andrea.pires@etu.univ-amu.fr>
2020-05-07 15:26:03 +0200

    modif de l'accelerometre

--- app/src/main/java/licence/projet/oblika/activities/GameActivity.java
+++ app/src/main/java/licence/projet/oblika/activities/GameActivity.java
@@ -2,6 +2,7 @@ package licence.projet.oblika.activities;
 
 import android.app.Activity;
 import android.content.Context;
+import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
@@ -18,8 +19,10 @@ public class GameActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         SensorManager sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
-        sensorManager.registerListener(accelerometer, SensorManager.SENSOR_ACCELEROMETER);
-
+        if (sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER).size() > 0) {
+            //si l'accéléromètre est présent
+            sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0);
+        }
         gLView = new GLView(this);
         setContentView(gLView);
     }
