d542774d23732aaef831bc913b8d7c94fc440186
JaiPasDidee <andrea.pires@etu.univ-amu.fr>
2020-05-15 16:11:36 +0200

    static get accelerometre

--- app/src/main/java/licence/projet/oblika/engine/utils/AccelerometerListener.java
+++ app/src/main/java/licence/projet/oblika/engine/utils/AccelerometerListener.java
@@ -7,7 +7,7 @@ import android.hardware.SensorListener;
 import android.hardware.SensorManager;
 
 public class AccelerometerListener implements SensorEventListener {
-    float x, y, z;
+    static float x, y, z;
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
@@ -22,15 +22,15 @@ public class AccelerometerListener implements SensorEventListener {
 
     }
 
-    public float getX() {
+    public static float getX() {
         return x;
     }
 
-    public float getY() {
+    public static float getY() {
         return y;
     }
 
-    public float getZ() {
+    public static float getZ() {
         return z;
     }
 }
