d781cfc17a0246c52865bb9ed7b09bb66198339f
CookieKastanie <jerem0205@yahoo.fr>
2020-05-24 16:26:17 +0200

    menu

--- app/src/main/java/licence/projet/oblika/activities/MainActivity.java
+++ app/src/main/java/licence/projet/oblika/activities/MainActivity.java
@@ -1,14 +1,14 @@
 package licence.projet.oblika.activities;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import licence.projet.oblika.R;
 
-public class  MainActivity extends AppCompatActivity {
+public class  MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
