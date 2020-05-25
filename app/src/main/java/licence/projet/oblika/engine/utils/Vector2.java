0b995e4ba160bc21e8a42e425a2ffbb477dd8fa2
TechnoShark13 <mathieu.dasilva13@gmail.com>
2020-05-25 16:18:43 +0200

    Modifications de Vector2

--- app/src/main/java/licence/projet/oblika/engine/utils/Vector2.java
+++ app/src/main/java/licence/projet/oblika/engine/utils/Vector2.java
@@ -40,4 +40,19 @@ public class Vector2 {
         }
     }
 
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
 }
