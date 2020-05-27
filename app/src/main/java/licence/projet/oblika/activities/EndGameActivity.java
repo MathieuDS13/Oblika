d0bc682b128f3f5783baff44789592263e54c29f
JaiPasDidee <andrea.pires@etu.univ-amu.fr>
2020-05-27 17:39:39 +0200

    image de fin qui renvoit au menu principal

---
+++ app/src/main/java/licence/projet/oblika/activities/EndGameActivity.java
@@ -0,0 +1,29 @@
+package licence.projet.oblika.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+
+import licence.projet.oblika.R;
+
+public class EndGameActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end);
+
+
+        Button buttonPrefActivity = findViewById(R.id.buttonReturn);
+        buttonPrefActivity.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent activity2Intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(activity2Intent);
+            }
+        });
+    }
+}
