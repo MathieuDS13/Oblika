1831fda75d60a112a2cf02907938b5c9039a9bcd
TechnoShark13 <mathieu.dasilva13@gmail.com>
2020-05-28 11:23:14 +0200

    yes

--- app/src/main/java/licence/projet/oblika/activities/LoginActivity.java
+++ app/src/main/java/licence/projet/oblika/activities/LoginActivity.java
@@ -42,9 +42,14 @@ public class LoginActivity extends Activity {
         buttonLogin.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-                String email = edMail.getText().toString();
-                String password = edPass.getText().toString();
-                login(email, password);
+                String email = edMail.getText().toString().trim();
+                String password = edPass.getText().toString().trim();
+                if (isInfoValid(email, password)) {
+                    login(email, password);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Invalid info.",
+                            Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -68,35 +73,37 @@ public class LoginActivity extends Activity {
 
 
     private boolean isInfoValid(String mail, String password) {
-        return false;
+        return mail != null && password != null;
     }
 
     private void login(String email, String password) {
         email.trim();
         password.trim();
         boolean infoIsValid = isInfoValid(email, password);
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            DataBaseHandler.login(user);
-                            //Renvoie sur la page d'accueil
-                            Intent activity2Intent = new Intent(getApplicationContext(), MainActivity.class);
-                            startActivity(activity2Intent);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            //TODO mettre à jour la page si l'utilisateur n'arrive pas à se connecter
+        if (infoIsValid) {
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "signInWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                DataBaseHandler.login(user);
+                                //Renvoie sur la page d'accueil
+                                Intent activity2Intent = new Intent(getApplicationContext(), MainActivity.class);
+                                startActivity(activity2Intent);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
                         }
-
-                        // ...
-                    }
-                });
+                    });
+        } else {
+            Toast.makeText(LoginActivity.this, "Invalid info.",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
